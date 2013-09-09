e73300213b403816fd759cd41373e0e69e7bea1c
Klaus Warndorf <klauswarndorf>
2013-09-09 18:03:24 +0200

    neuer stand

--- src/main/java/com/yoc/mte/hbase/viewAdTechImpl.java
+++ src/main/java/com/yoc/mte/hbase/viewAdTechImpl.java
@@ -3,8 +3,6 @@
  */
 package com.yoc.mte.hbase;
 
-import java.math.BigInteger;
-
 import org.apache.camel.dataformat.bindy.annotation.CsvRecord;
 import org.apache.camel.dataformat.bindy.annotation.DataField;
 
@@ -13,14 +11,14 @@ import org.apache.camel.dataformat.bindy.annotation.DataField;
  * @author klaus
  *
  */
-@CsvRecord( separator = ",", isOrdered = true )
-public class viewAdTechImpl implements viewAdTech {
+@CsvRecord( separator = "," )
+public class viewAdTechImpl {
 
     @DataField(pos = 1, position = 1)
-    private String sequenceId;
+    public String sequenceId;
 
     @DataField(pos = 2, position = 2)
-    private String plcNetworkId;
+    public String plcNetworkId;
 
     @DataField(pos = 3, position = 3)
     private String plcSubNetworkId;
@@ -40,131 +38,71 @@ public class viewAdTechImpl implements viewAdTech {
     @DataField(pos = 8, position = 8)
     private String kvString;
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getSequenceId()
-     */
-    @Override
     public String getSequenceId() {
         return sequenceId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setSequenceId(java.lang.String)
-     */
-    @Override
     public void setSequenceId(String sequenceId) {
         this.sequenceId = sequenceId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getPlcNetworkId()
-     */
-    @Override
     public String getPlcNetworkId() {
         return plcNetworkId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setPlcNetworkId(java.lang.String)
-     */
-    @Override
     public void setPlcNetworkId(String plcNetworkId) {
         this.plcNetworkId = plcNetworkId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getPlcSubNetworkId()
-     */
-    @Override
     public String getPlcSubNetworkId() {
         return plcSubNetworkId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setPlcSubNetworkId(java.lang.String)
-     */
-    @Override
     public void setPlcSubNetworkId(String plcSubNetworkId) {
         this.plcSubNetworkId = plcSubNetworkId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getWebsiteId()
-     */
-    @Override
     public String getWebsiteId() {
         return websiteId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setWebsiteId(java.lang.String)
-     */
-    @Override
     public void setWebsiteId(String websiteId) {
         this.websiteId = websiteId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getPlacementId()
-     */
-    @Override
     public String getPlacementId() {
         return placementId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setPlacementId(java.lang.String)
-     */
-    @Override
     public void setPlacementId(String placementId) {
         this.placementId = placementId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getCampaignId()
-     */
-    @Override
     public String getCampaignId() {
         return campaignId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setCampaignId(java.lang.String)
-     */
-    @Override
     public void setCampaignId(String campaignId) {
         this.campaignId = campaignId;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getExtensionType()
-     */
-    @Override
     public String getExtensionType() {
         return extensionType;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setExtensionType(java.lang.String)
-     */
-    @Override
     public void setExtensionType(String extensionType) {
         this.extensionType = extensionType;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#getKvString()
-     */
-    @Override
     public String getKvString() {
         return kvString;
     }
 
-    /* (non-Javadoc)
-     * @see com.yoc.mte.statistics.importcsv.viewAdTech#setKvString(java.lang.String)
-     */
-    @Override
     public void setKvString(String kvString) {
         this.kvString = kvString;
+    }
+
+    public String _toString() {
+        return "SequenceId: " +sequenceId;
     }
 }
