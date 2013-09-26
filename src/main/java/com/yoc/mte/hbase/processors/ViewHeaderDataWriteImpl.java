52e840a0f4e9a562be593a3e51ca1a78cfd9d13c
Klaus Warndorf <klauswarndorf>
2013-09-26 17:10:40 +0200

    scan and put

--- src/main/java/com/yoc/mte/hbase/processors/ViewHeaderDataWriteImpl.java
+++ src/main/java/com/yoc/mte/hbase/processors/ViewHeaderDataWriteImpl.java
@@ -3,13 +3,16 @@
  */
 package com.yoc.mte.hbase.processors;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
+import org.springframework.beans.DirectFieldAccessor;
 
-import com.yoc.mte.hbase.helper.CamelHbaseSetHeader;
+import com.yoc.mte.hbase.viewMapping.HbaseData;
 import com.yoc.mte.hbase.viewMapping.ViewAdTechImpl;
 
 /**
@@ -17,7 +20,7 @@ import com.yoc.mte.hbase.viewMapping.ViewAdTechImpl;
  *
  * processor to write data with camel-hbase
  */
-public class ViewHeaderDataWriteImpl extends CamelHbaseSetHeader implements Processor {
+public class ViewHeaderDataWriteImpl implements Processor {
 
     @Override
     public void process(Exchange exchange) throws Exception {
@@ -32,6 +35,32 @@ public class ViewHeaderDataWriteImpl extends CamelHbaseSetHeader implements Proc
                 viewAdTechImpl.setId(viewAdTechImpl.getSequenceId() + "_" + viewAdTechImpl.getPlcNetworkId() + "_" + viewAdTechImpl.getKvYrid());
                 // set header for camel-hbase
                 this.setHeaderData(exchange, viewAdTechImpl, "cf");
+            }
+        }
+    }
+
+
+    private void setHeaderData(Exchange exchange, HbaseData data, String columnFamily) {
+
+        // counter for header postfix
+        int i = 0;
+        for (Field field : ViewAdTechImpl.class.getDeclaredFields()) {
+            if (!Arrays.asList(new String[] { "serialVersionUID", "id" }).contains(field.getName())) {
+
+                DirectFieldAccessor myObjectAccessor = new DirectFieldAccessor(data);
+
+                // sets a postfix as camel-hbase expects for header data
+                String pf = "";
+                if (i != 0) {
+                    pf = String.valueOf(i);
+                }
+                // sets header data for camel-hbase
+                exchange.getIn().setHeader("CamelHBaseRowId" + pf, data.getId());
+                exchange.getIn().setHeader("CamelHBaseFamily" + pf, columnFamily);
+                exchange.getIn().setHeader("CamelHBaseQualifier" + pf, field.getName());
+                exchange.getIn().setHeader("CamelHBaseValue" + pf, (String) myObjectAccessor.getPropertyValue(field.getName()));
+                // increment counter
+                i++;
             }
         }
     }
